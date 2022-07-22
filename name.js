1d2300a8dbb896e1a857d4fdfd89f52d064551e1
Aislu Baisova <ice_lu_93@mail.ru>
2022-07-22 20:53:42 +0300

    Delete name.js

--- name.js
+++
@@ -1,8 +0,0 @@
-function inTerminal(name) {
-if (name == 'Aislu') {
-    console.log('Hello ' + name + '!') }
-    else console.log('There is no such name!')
-}
-
-inTerminal('Oleg')
-inTerminal('Aislu')

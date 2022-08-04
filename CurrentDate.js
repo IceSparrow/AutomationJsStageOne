52c83c5f7d85edf0a8b90e1eaa1f0a978958c770
Aislu Baisova <ice_lu_93@mail.ru>
2022-08-04 18:26:32 +0300

    3d task done

---
+++ CurrentDate.js
@@ -0,0 +1,5 @@
+let date = new Date() 
+
+console.log("Today is " + date.getDate() + "th day")
+console.log("of the " + (date.getMonth() +1) + "th month!")
+console.log("The year is " + date.getFullYear() + "!")
